8cc3d865a9efaf428d2b4a8e570ac20e464a7313
dimitrykorno <32943435+dimitrykorno@users.noreply.github.com>
2018-10-16 17:04:35 +0300

    __slots__

--- KCAnalytics/Classes/Events.py
+++ KCAnalytics/Classes/Events.py
@@ -1,109 +1,98 @@
-from In_apps.In_apps import check_inapp_name, get_price
+from In_apps.Shop import check_inapp_name, get_price
+from report_api.Classes.Events import *
 
 
-class Event:
-    not_found = set()
+class KCEvent(Event):
+    __slots__ = 'obj_name'
 
     def __init__(self, obj_name, datetime):
+        super().__init__(datetime)
         self.obj_name = obj_name
-        self.datetime = datetime
 
     def to_string(self):
         return "Other event: " + str(self.obj_name)
 
 
-class KC_ReadEvent(Event):
+class KCReadEvent(KCEvent):
     def __init__(self, book_name, datetime):
-        Event.__init__(self, book_name, datetime)
+        super().__init__(book_name, datetime)
 
     def to_string(self):
         return "Read Event:  " + self.obj_name
 
 
-class KC_ReadFree(Event):
+class KCReadFree(KCEvent):
     def __init__(self, book_name, datetime):
-        Event.__init__(self, book_name, datetime)
+        super().__init__(book_name, datetime)
 
     def to_string(self):
         return "Read Free:   " + self.obj_name
 
 
-class KC_BuyEvent(Event):
-    def __init__(self, bought_object, datetime):
-        Event.__init__(self, check_inapp_name(bought_object), datetime)
-
-    @property
-    def price(self):
-        return get_price(self.obj_name)
+class KCBuyEvent(PurchaseEvent):
+    def __init__(self, purchase, price, status, datetime):
+        super().__init__(check_inapp_name(purchase), status, price, datetime)
 
     def to_string(self):
-        return "Buy Event:   " + self.obj_name
+        return "Buy Event:   " + self.purchase
 
 
-class KC_BuyCancel(Event):
-    def __init__(self, bought_object, datetime):
-        Event.__init__(self, check_inapp_name(bought_object), datetime)
-
-    @property
-    def price(self):
-        return get_price(self.obj_name)
+class KCBuyCancel(PurchaseEvent):
+    def __init__(self, purchase, price, status, datetime):
+        super().__init__(check_inapp_name(purchase), status, price, datetime)
 
     def to_string(self):
-        return "Buy Cancel:  " + self.obj_name
+        return "Buy Cancel:  " + self.purchase
 
 
-class KC_BuyFailed(Event):
-    def __init__(self, bought_object, datetime):
-        Event.__init__(self, check_inapp_name(bought_object), datetime)
-
-    @property
-    def price(self):
-        return get_price(self.obj_name)
+class KCBuyFailed(PurchaseEvent):
+    def __init__(self, purchase, price, status, datetime):
+        super().__init__(check_inapp_name(purchase), status, price, datetime)
 
     def to_string(self):
-        return "Buy Failed:  " + self.obj_name
+        return "Buy Failed:  " + self.purchase
 
 
-class KC_BuyTap(Event):
+class KCBuyTap(KCEvent):
     def __init__(self, bought_object, datetime):
-        Event.__init__(self, check_inapp_name(bought_object), datetime)
+        super().__init__(check_inapp_name(bought_object), datetime)
 
     def to_string(self):
         return "Buy Tap:     " + self.obj_name
 
 
-class KC_TapShelf(Event):
+class KCTapShelf(KCEvent):
     def __init__(self, shelf, datetime):
-        Event.__init__(self, shelf, datetime)
+        super().__init__(shelf, datetime)
 
     def to_string(self):
         return "Enter shelf " + self.obj_name
 
 
-class KC_TapPresent(Event):
+class KCTapPresent(Event):
     def __init__(self, datetime):
-        Event.__init__(self, "Tap_present", datetime)
+        super().__init__(datetime)
 
     def to_string(self):
-        return "Get Present: " + self.obj_name
+        return "Tap Present"
 
 
-class KC_TapBook(Event):
+class KCTapBook(KCEvent):
     def __init__(self, book_name, datetime):
-        Event.__init__(self, check_inapp_name(book_name), datetime)
+        super().__init__(check_inapp_name(book_name), datetime)
 
     def to_string(self):
         return "Tap Book:    " + self.obj_name
 
 
-class KC_ParentGate(Event):
+class KCParentGate(Event):
     def __init__(self, datetime):
-        Event.__init__(self, "Parent_gate", datetime)
+        super().__init__(datetime)
 
     def to_string(self):
-        return "Parent gate: " + self.obj_name
+        return "Parent gate"
 
 
-class KC_CRASH(Event):
+class KCCRASH(Event):
     def __init__(self, datetime):
-        Event.__init__(self, "CRASH EVENT", datetime)
+        super().__init__(datetime)
